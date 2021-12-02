723bc0c190b50aa0f6d73b3e2a0fc7aa6ccddc79
Phong Hoang <haiphong1712@gmail.com>
2021-12-02 13:04:51 -0800

    setup client side with create-react-app and firebase config

---
+++ client/src/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAEAZK1Ojs1qpnhj9UBgwwx3t_R5UmLCrw',
+  authDomain: 'challenge-f565e.firebaseapp.com',
+  projectId: 'challenge-f565e',
+  storageBucket: 'challenge-f565e.appspot.com',
+  messagingSenderId: '967921048245',
+  appId: '1:967921048245:web:e2803c21ac9acab28b3cfc',
+  measurementId: 'G-L0RRP2G2VP',
+};
+
+export default firebaseConfig;
