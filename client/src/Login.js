8b8b9b8f8f451d8b7cb916d3b85efb65cff457dc
Phong Hoang <haiphong1712@gmail.com>
2021-12-03 19:02:30 -0800

    add state for the Login component

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 import { Link } from 'react-router-dom';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const signIn = (e) => {
+    e.preventDefault();
+  };
+
+  const register = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className='login'>
       <Link to='/'>
@@ -17,12 +28,25 @@ function Login() {
         <h1>Sign-in</h1>
         <form>
           <h5>E-mail</h5>
-          <input type='text' />
-
+          <input
+            type='text'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <h5>Password</h5>
-          <input type='password' />
+          <input
+            type='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-          <button className='login__signInButton'>Sign In</button>
+          <button
+            type='submit'
+            onClick={signIn}
+            className='login__signInButton'
+          >
+            Sign In
+          </button>
         </form>
 
         <p>
@@ -31,7 +55,7 @@ function Login() {
           Interest-Based Ads Notice.
         </p>
 
-        <button className='login__registerButton'>
+        <button onClick={register} className='login__registerButton'>
           Create your Amazon Account
         </button>
       </div>
